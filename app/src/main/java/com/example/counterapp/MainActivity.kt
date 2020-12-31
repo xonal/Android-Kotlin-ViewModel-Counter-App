6047f2d556422fef77ca4ce2ce7c0691fdc3cf27
xonal <xonalluck@gmail.com>
2020-12-31 03:11:17 -0800

    comments

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -12,22 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var textTimer = findViewById<TextView>(R.id.textTimer)
-        var btnAdd = findViewById<Button>(R.id.button_add)
-        var btnReset = findViewById<Button>(R.id.button_reset)
+        var textTimer = findViewById<TextView>(R.id.textTimer) //define text
+        var btnAdd = findViewById<Button>(R.id.button_add) //define add button
+        var btnReset = findViewById<Button>(R.id.button_reset) //define reset button
 
-        val viewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+        val viewModel = ViewModelProvider(this).get(ActivityViewModel::class.java) //initialize viewModel
 
-        textTimer.text = viewModel.number.toString()
+        textTimer.text = viewModel.number.toString() //set text timer with number variable in viewModel
 
-        btnAdd.setOnClickListener {
-            viewModel.addNumber()
-            textTimer.text = viewModel.number.toString()
+        btnAdd.setOnClickListener { //action click to add number
+            viewModel.addNumber() //call function addNumber() in viewModel
+            textTimer.text = viewModel.number.toString() //set text timer with number variable in viewModel
         }
 
         btnReset.setOnClickListener {
-            viewModel.resetNumber()
-            textTimer.text = viewModel.number.toString()
+            viewModel.resetNumber() //call function resetNumber() in viewModel
+            textTimer.text = viewModel.number.toString() //set text timer with number variable in viewModel
         }
 
     }
