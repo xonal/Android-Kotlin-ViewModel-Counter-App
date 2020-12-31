6cea5f83d63b14a00e8f59edf10aad181b18e96b
xonal <xonalluck@gmail.com>
2020-12-31 03:09:01 -0800

    ActivityViewModel

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var textTimer = findViewById<TextView>(R.id.textTimer)
+        var btnAdd = findViewById<Button>(R.id.button_add)
+        var btnReset = findViewById<Button>(R.id.button_reset)
+
+        val viewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
+
+        textTimer.text = viewModel.number.toString()
+
+        btnAdd.setOnClickListener {
+            viewModel.addNumber()
+            textTimer.text = viewModel.number.toString()
+        }
+
+        btnReset.setOnClickListener {
+            viewModel.resetNumber()
+            textTimer.text = viewModel.number.toString()
+        }
+
     }
 }
